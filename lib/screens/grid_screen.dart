d3790298e318bc82949c4d1fac4cc449597a7b3c
Attila Vago <attilavago85@gmail.com>
2020-08-23 15:07:23 +0100

    show grid items on grid screen dynamically

--- lib/screens/grid_screen.dart
+++ lib/screens/grid_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:give_me_voice/models/grid_list.dart';
 import 'package:give_me_voice/models/grid_data.dart';
 import 'package:provider/provider.dart';
+import 'package:give_me_voice/components/round_button.dart';
+import 'package:give_me_voice/components/round_button_label.dart';
 
 class GridScreen extends StatelessWidget {
   static String id = 'grid_screen';
@@ -15,6 +17,38 @@ class GridScreen extends StatelessWidget {
           title: Text(Provider.of<GridItemData>(context)
               .gridLists[gridData.activeCardIndex]
               .gridName),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.builder(
+                    itemCount: gridData
+                        .gridLists[gridData.activeCardIndex].items.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      final item = gridData
+                          .gridLists[gridData.activeCardIndex].items[index];
+                      return Column(
+                        children: [
+                          RoundButton(
+                            imageName: item.image,
+                            size: 70.0,
+                            padding: 15.0,
+                            buttonAction: () {},
+                            buttonLongAction: () {},
+                          ),
+                          RoundButtonLabel(
+                            label: item.label,
+                          )
+                        ],
+                      );
+                    }),
+              ),
+            ],
+          ),
         ),
       );
     });
