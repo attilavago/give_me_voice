5f996d29596db75a1ff7b8c67d699c8f987456ee
attila vago <attilavago85@gmail.com>
2021-06-11 15:07:43 +0100

    small styling changes

--- lib/screens/grids_screen.dart
+++ lib/screens/grids_screen.dart
@@ -16,6 +16,7 @@ class _GridsScreenState extends State<GridsScreen> {
       MyGridsDataBaseConnector();
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     _myGridsDataBaseConnector.initializeDatabase().then((value) {});
     Future<void> _showMyDialog(gridId, gridName, context) async {
       return showDialog<void>(
@@ -63,128 +64,134 @@ class _GridsScreenState extends State<GridsScreen> {
             return Padding(
               padding: const EdgeInsets.all(10.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   if (gridsCount == 0)
                     Text('You currently have no grids. How about adding one?',
                         textAlign: TextAlign.center),
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: gridsCount,
-                        itemBuilder: (context, index) {
-                          final grid = grids[index];
-                          final gridImages = grid['images'].split(',');
-                          return Dismissible(
-                            key: UniqueKey(),
-                            direction: DismissDirection.endToStart,
-                            onDismissed: (direction) {
-                              setState(() {
-                                _myGridsDataBaseConnector
-                                    .deleteGrid(grid['id']);
-                              });
-                              Scaffold.of(context).showSnackBar(SnackBar(
-                                  content: Text(
-                                      'The "${grid['name'].toString().capitalize()}" grid has been deleted!')));
-                            },
-                            background: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(10.0),
-                                  topLeft: Radius.circular(10.0),
-                                  bottomLeft: Radius.circular(10.0),
-                                  bottomRight: Radius.circular(10.0),
-                                ),
-                              ),
-                              padding: EdgeInsets.all(30.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    'delete',
-                                    textAlign: TextAlign.right,
-                                    style: TextStyle(color: Colors.white),
+                  if (gridsCount > 0)
+                    Expanded(
+                      child: ListView.builder(
+                          itemCount: gridsCount,
+                          itemBuilder: (context, index) {
+                            final grid = grids[index];
+                            final gridImages = grid['images'].split(',');
+                            return Dismissible(
+                              key: UniqueKey(),
+                              direction: DismissDirection.endToStart,
+                              onDismissed: (direction) {
+                                setState(() {
+                                  _myGridsDataBaseConnector
+                                      .deleteGrid(grid['id']);
+                                });
+                                Scaffold.of(context).showSnackBar(SnackBar(
+                                    content: Text(
+                                        'The "${grid['name'].toString().capitalize()}" grid has been deleted!')));
+                              },
+                              background: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(10.0),
+                                    topLeft: Radius.circular(10.0),
+                                    bottomLeft: Radius.circular(10.0),
+                                    bottomRight: Radius.circular(10.0),
                                   ),
-                                ],
-                              ),
-                            ),
-                            child: Card(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
+                                ),
+                                padding: EdgeInsets.all(30.0),
                                 child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
                                   children: [
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          grid['name'].toString().capitalize(),
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline6,
-                                        ),
-                                        IconButton(
-                                          onPressed: () {
-                                            _showMyDialog(grid['id'],
-                                                    grid['name'], context)
-                                                .then((_) => setState(() {}));
-                                          },
-                                          icon: Icon(Icons.delete),
-                                          color: Theme.of(context).accentColor,
-                                        ),
-                                      ],
+                                    Text(
+                                      'delete',
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(color: Colors.white),
                                     ),
-                                    Row(
-                                      children: [
-                                        for (var image in gridImages)
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                right: 3.0, bottom: 10.0),
-                                            child: Image.asset(
-                                              'images/${image}.png',
-                                              height: 30.0,
-                                            ),
-                                          )
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        OutlineButton(
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(18.0)),
-                                          onPressed: () {
-                                            gridData
-                                                .setActiveCardIndex(grid['id']);
-                                            Navigator.pushNamed(
-                                                context, GridScreen.id);
-                                          },
-                                          child: Text(
-                                            'Open',
-                                            style: TextStyle(fontSize: 17.0),
-                                          ),
-                                          color: Theme.of(context).accentColor,
-                                          highlightedBorderColor:
-                                              Theme.of(context).accentColor,
-                                          borderSide: BorderSide(
-                                              color: Theme.of(context)
-                                                  .accentColor),
-                                          textColor:
-                                              Theme.of(context).accentColor,
-                                          padding: EdgeInsets.symmetric(
-                                              vertical: 0, horizontal: 1.0),
-                                        ),
-                                      ],
-                                    )
                                   ],
                                 ),
                               ),
-                            ),
-                          );
-                        }),
-                  )
+                              child: Card(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            grid['name']
+                                                .toString()
+                                                .capitalize(),
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headline6,
+                                          ),
+                                          IconButton(
+                                            onPressed: () {
+                                              _showMyDialog(grid['id'],
+                                                      grid['name'], context)
+                                                  .then((_) => setState(() {}));
+                                            },
+                                            icon: Icon(Icons.delete),
+                                            color:
+                                                Theme.of(context).accentColor,
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          for (var image in gridImages)
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  right: 3.0, bottom: 10.0),
+                                              child: Image.asset(
+                                                'images/${image}.png',
+                                                height: 30.0,
+                                              ),
+                                            )
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          OutlineButton(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        18.0)),
+                                            onPressed: () {
+                                              gridData.setActiveCardIndex(
+                                                  grid['id']);
+                                              Navigator.pushNamed(
+                                                  context, GridScreen.id);
+                                            },
+                                            child: Text(
+                                              'Open',
+                                              style: TextStyle(fontSize: 17.0),
+                                            ),
+                                            color:
+                                                Theme.of(context).accentColor,
+                                            highlightedBorderColor:
+                                                Theme.of(context).accentColor,
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .accentColor),
+                                            textColor:
+                                                Theme.of(context).accentColor,
+                                            padding: EdgeInsets.symmetric(
+                                                vertical: 0, horizontal: 1.0),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
+                    )
                 ],
               ),
             );
@@ -192,7 +199,7 @@ class _GridsScreenState extends State<GridsScreen> {
             return Padding(
               padding: const EdgeInsets.all(10.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Text('Loading grids...', textAlign: TextAlign.center),
